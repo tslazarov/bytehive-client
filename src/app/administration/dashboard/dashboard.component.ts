fbe13e57131f0fdb83ac01efdaa5c22f53064234
tslazarov <lazarov.tsvetoslav@gmail.com>
2020-05-25 20:57:33 +0300

    Requests bar chart

--- src/app/administration/dashboard/dashboard.component.ts
+++ src/app/administration/dashboard/dashboard.component.ts
@@ -5,6 +5,7 @@ import { Constants } from '../../utilities/constants';
 import { MomentDateAdapter, MAT_MOMENT_DATE_FORMATS } from '@angular/material-moment-adapter';
 import { Router } from '@angular/router';
 import { ExportType } from '../../models/enums/exporttype.enum';
+import { formatDate } from '@angular/common';
 
 @Component({
     selector: 'app-dashboard',
@@ -22,9 +23,11 @@ export class DashboardComponent implements OnInit {
     summary: any;
     users: any;
     requests: any;
+    requestsChartData: any[];
     exportUsageChartData: any[];
 
     colorSchemeChart = { domain: ['#F44336', '#2196F3', '#FFEB3B', '#4CAF50', '#9C27B0', '#FF9800'] };
+    colorSchemeBarChart = { domain: ['#FFEB3B'] };
 
 
     constructor(private router: Router,
@@ -55,11 +58,11 @@ export class DashboardComponent implements OnInit {
             .subscribe((result) => {
                 this.requests = result;
                 this.bindExportUsageData(result);
-                console.log(this.exportUsageChartData);
+                this.bindVerticalChart(result);
             });
     }
 
-    bindExportUsageData(requests): void {
+    bindExportUsageData(requests: any[]): void {
         this.exportUsageChartData = [{ name: 'test', type: ExportType.Csv, value: 0 },
         { name: 'test1', type: ExportType.Json, value: 0 },
         { name: 'test2', type: ExportType.Xml, value: 0 },
@@ -73,6 +76,64 @@ export class DashboardComponent implements OnInit {
                 req.value += 1;
             }
         }
+    }
+
+    bindVerticalChart(requests: any[]) {
+
+        this.requestsChartData = [];
+
+        for (let i = 0; i < 30; i++) {
+            let currentDate = new Date();
+            let normalizedDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+            normalizedDate.setDate(normalizedDate.getDate() - i);
+
+            let pointsDataEntry = { name: formatDate(normalizedDate, 'dd.MM', 'bg'), series: [] };
+
+            this.requestsChartData.push(pointsDataEntry);
+        }
+
+        let daysPoints = {};
+
+        for (const request of requests) {
+            if (request.creationDate) {
+                console.log(request.creationDate);
+                let originalDate = new Date(request.creationDate);
+                let normalizedDate = new Date(originalDate.getFullYear(), originalDate.getMonth(), originalDate.getDate());
+
+                if (daysPoints[formatDate(normalizedDate, 'dd.MM', 'bg')] >= 0) {
+                    daysPoints[formatDate(normalizedDate, 'dd.MM', 'bg')] += 1;
+                }
+                else {
+                    daysPoints[formatDate(normalizedDate, 'dd.MM', 'bg')] = 1;
+                }
+            }
+        }
+
+        for (const pointKey in daysPoints) {
+            if (daysPoints.hasOwnProperty(pointKey)) {
+                let pointValue = daysPoints[pointKey];
+
+                var point = this.requestsChartData.find(obj => {
+                    return obj.name === pointKey
+                });
+
+                if (point && point.series) {
+                    let cartnerElement = { name: '', value: 0 };
+
+                    cartnerElement.name = 'test';
+                    cartnerElement.value = Math.round(pointValue);
+
+                    point.series.push(cartnerElement);
+                }
+            }
+        }
+
+        this.requestsChartData.reverse();
+    }
+
+    yAxisTickFormatting(value) {
+        console.log(value);
+        return Math.round(value);
     }
 
     navigate(route: string): void {
