975a8578219588977125a3511b191dbd17a44ed5
tslazarov <lazarov.tsvetoslav@gmail.com>
2020-05-25 21:19:43 +0300

    Dashboard localization

--- src/app/administration/dashboard/dashboard.component.ts
+++ src/app/administration/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DateAdapter, MAT_DATE_LOCALE, MAT_DATE_FORMATS } from 'saturn-datepicker';
 import { StatisticsService } from '../../services/statistics.service';
 import { Constants } from '../../utilities/constants';
@@ -6,6 +6,9 @@ import { MomentDateAdapter, MAT_MOMENT_DATE_FORMATS } from '@angular/material-mo
 import { Router } from '@angular/router';
 import { ExportType } from '../../models/enums/exporttype.enum';
 import { formatDate } from '@angular/common';
+import { TranslationService } from '../../services/utilities/translation.service';
+import { Subscription } from 'rxjs';
+import { CommunicationService } from '../../services/utilities/communication.service';
 
 @Component({
     selector: 'app-dashboard',
@@ -17,7 +20,10 @@ import { formatDate } from '@angular/common';
         { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS }
     ]
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
+
+    // subscriptions
+    languageChangeSubscription: Subscription;
 
     // common
     summary: any;
@@ -29,14 +35,52 @@ export class DashboardComponent implements OnInit {
     colorSchemeChart = { domain: ['#F44336', '#2196F3', '#FFEB3B', '#4CAF50', '#9C27B0', '#FF9800'] };
     colorSchemeBarChart = { domain: ['#FFEB3B'] };
 
+    // labels
+    usersLabel: string;
+    requestsLabel: string;
+    entriesCountLabel: string;
+    earningsLabel: string;
+    currencyLabel: string;
+    requestsLast30DaysLabel: string;
+    exportTypesLabel: string;
+    last5UsersLabel: string;
+    last5RequestsLabel: string;
+    byLabel: string;
+    viewAllLabel: string;
 
     constructor(private router: Router,
-        private statisticsService: StatisticsService) { }
+        private communicationService: CommunicationService,
+        private statisticsService: StatisticsService,
+        private translationService: TranslationService) { }
 
     ngOnInit(): void {
         this.fetchSummary();
         this.fetchUsersSummary();
         this.fetchRequestsSummary();
+
+        this.setLabelsMessages();
+
+        this.languageChangeSubscription = this.communicationService.languageChangeEmitted.subscribe(() => {
+            this.setLabelsMessages();
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.languageChangeSubscription.unsubscribe();
+    }
+
+    setLabelsMessages(): void {
+        this.usersLabel = this.translationService.localizeValue('usersLabel', 'dashboard', 'label');
+        this.requestsLabel = this.translationService.localizeValue('requestsLabel', 'dashboard', 'label');
+        this.entriesCountLabel = this.translationService.localizeValue('entriesCountLabel', 'dashboard', 'label');
+        this.earningsLabel = this.translationService.localizeValue('earningsLabel', 'dashboard', 'label');
+        this.currencyLabel = this.translationService.localizeValue('currencyLabel', 'dashboard', 'label');
+        this.requestsLast30DaysLabel = this.translationService.localizeValue('requestsLast30DaysLabel', 'dashboard', 'label');
+        this.exportTypesLabel = this.translationService.localizeValue('exportTypesLabel', 'dashboard', 'label');
+        this.last5UsersLabel = this.translationService.localizeValue('last5UsersLabel', 'dashboard', 'label');
+        this.last5RequestsLabel = this.translationService.localizeValue('last5RequestsLabel', 'dashboard', 'label');
+        this.byLabel = this.translationService.localizeValue('byLabel', 'dashboard', 'label');
+        this.viewAllLabel = this.translationService.localizeValue('viewAllLabel', 'dashboard', 'label');
     }
 
     fetchSummary(): void {
@@ -63,10 +107,10 @@ export class DashboardComponent implements OnInit {
     }
 
     bindExportUsageData(requests: any[]): void {
-        this.exportUsageChartData = [{ name: 'test', type: ExportType.Csv, value: 0 },
-        { name: 'test1', type: ExportType.Json, value: 0 },
-        { name: 'test2', type: ExportType.Xml, value: 0 },
-        { name: 'test3', type: ExportType.Txt, value: 0 },
+        this.exportUsageChartData = [{ name: 'CSV', type: ExportType.Csv, value: 0 },
+        { name: 'JSON', type: ExportType.Json, value: 0 },
+        { name: 'XML', type: ExportType.Xml, value: 0 },
+        { name: 'TXT', type: ExportType.Txt, value: 0 },
         ];
 
         for (const request of requests) {
@@ -87,7 +131,7 @@ export class DashboardComponent implements OnInit {
             let normalizedDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
             normalizedDate.setDate(normalizedDate.getDate() - i);
 
-            let pointsDataEntry = { name: formatDate(normalizedDate, 'dd.MM', 'bg'), series: [] };
+            let pointsDataEntry = { name: formatDate(normalizedDate, 'dd.MM', this.translationService.getLanguage()), series: [] };
 
             this.requestsChartData.push(pointsDataEntry);
         }
@@ -96,15 +140,14 @@ export class DashboardComponent implements OnInit {
 
         for (const request of requests) {
             if (request.creationDate) {
-                console.log(request.creationDate);
                 let originalDate = new Date(request.creationDate);
                 let normalizedDate = new Date(originalDate.getFullYear(), originalDate.getMonth(), originalDate.getDate());
 
-                if (daysPoints[formatDate(normalizedDate, 'dd.MM', 'bg')] >= 0) {
-                    daysPoints[formatDate(normalizedDate, 'dd.MM', 'bg')] += 1;
+                if (daysPoints[formatDate(normalizedDate, 'dd.MM', this.translationService.getLanguage())] >= 0) {
+                    daysPoints[formatDate(normalizedDate, 'dd.MM', this.translationService.getLanguage())] += 1;
                 }
                 else {
-                    daysPoints[formatDate(normalizedDate, 'dd.MM', 'bg')] = 1;
+                    daysPoints[formatDate(normalizedDate, 'dd.MM', this.translationService.getLanguage())] = 1;
                 }
             }
         }
@@ -118,12 +161,12 @@ export class DashboardComponent implements OnInit {
                 });
 
                 if (point && point.series) {
-                    let cartnerElement = { name: '', value: 0 };
+                    let requestsElement = { name: '', value: 0 };
 
-                    cartnerElement.name = 'test';
-                    cartnerElement.value = Math.round(pointValue);
+                    requestsElement.name = 'DayChart';
+                    requestsElement.value = Math.round(pointValue);
 
-                    point.series.push(cartnerElement);
+                    point.series.push(requestsElement);
                 }
             }
         }
@@ -132,7 +175,6 @@ export class DashboardComponent implements OnInit {
     }
 
     yAxisTickFormatting(value) {
-        console.log(value);
         return Math.round(value);
     }
 
--- src/app/utilities/labels-messages.ts
+++ src/app/utilities/labels-messages.ts
@@ -790,6 +790,52 @@ export class LabelsMessages {
                 'bg': 'Сигурни ли сте, че искате да изтриете този потребител?'
             }
         },
+        'dashboard': {
+            'usersLabel': {
+                'en': 'users',
+                'bg': 'потребители'
+            },
+            'requestsLabel': {
+                'en': 'requests',
+                'bg': 'заявки'
+            },
+            'entriesCountLabel': {
+                'en': 'entries',
+                'bg': 'резултати'
+            },
+            'earningsLabel': {
+                'en': 'earnings',
+                'bg': 'печалба'
+            },
+            'currencyLabel': {
+                'en': 'eur',
+                'bg': 'евро'
+            },
+            'requestsLast30DaysLabel': {
+                'en': 'Requests for the last 30 days',
+                'bg': 'Заявки за последните 30 дни'
+            },
+            'exportTypesLabel': {
+                'en': 'Export types',
+                'bg': 'Експортирани типове'
+            },
+            'last5UsersLabel': {
+                'en': 'Last 5 users',
+                'bg': 'Последни 5 потребители'
+            },
+            'last5RequestsLabel': {
+                'en': 'Last 5 requests',
+                'bg': 'Последни 5 заявки'
+            },
+            'byLabel': {
+                'en': 'by',
+                'bg': 'от'
+            },
+            'viewAllLabel': {
+                'en': 'View all',
+                'bg': 'Виж всички'
+            }
+        },
         'usersdetail': {
             'requestsLabel': {
                 'en': 'Requests',
